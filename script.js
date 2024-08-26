9e4d3bacb45d8b00f973d7399756cfb0d8d20721
Survey845 <bhanjaimukund@gmail.com>
2024-08-26 08:17:42 +0530

    A UI for my RPS game.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 console.log("Rock Paper Scissors");
 
-let humanScore, computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+
+
 
 function getComputerChoice(){
     let element;
@@ -16,46 +19,61 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let h_choice = prompt("What element do you throw, human?: ");
-    return h_choice.toUpperCase();
+
 }
 
-function playRound(humanChoice, computerChoice){
+function playRound(humanChoice){
+    let computerChoice = getComputerChoice();
+    let resultString;
     if(humanChoice==computerChoice){
-        console.log("Both chose the same element, nobody gains a point!");
+        resultString = "Both chose the same element, nobody gains a point!";
     } else if(humanChoice=="ROCK"){
         if(computerChoice=="PAPER"){
-            console.log("You lose, Paper beats Rock!");
+            resultString = "You lose, Paper beats Rock!";
             computerScore++;
         } else{
-            console.log("You win, Rock beats Scissor!");
+            resultString = "You win, Rock beats Scissor!";
             humanScore++;
         } 
     } else if(humanChoice=="PAPER"){
             if(computerChoice=="ROCK"){
-                console.log("You win, Paper beats Rock!");
+                resultString = "You win, Paper beats Rock!";
                 humanScore++;
             } else{
-                console.log("You lose, Scissors beat Paper!");
+                resultString = "You lose, Scissors beat Paper!";
                 computerScore++;
             }
     } else{
         if(computerChoice=="ROCK"){
-            console.log("You lose, Rock beats Scissors!");
+            resultString = "You lose, Rock beats Scissors!";
             computerScore++;
         } else{
-            console.log("You win, Scissors beats Paper!!");
+            resultString = "You win, Scissors beats Paper!!";
             humanScore++;
         }
     }
+    if(computerScore<5 && humanScore<5){
+    resultString += "<br/> Your Score: " + humanScore + "/ Computer Score: "+ computerScore;
+    } else if(computerScore==5){
+        resultString += "<br/> You Lose :((, Computer has 5 points";
+        computerScore = 0;
+        humanScore = 0;
+    } else{
+        resultString += "<br/> You Win!!!, You have 5 points! :)";
+        computerScore = 0;
+        humanScore = 0;
+    }
+    
+    return resultString;
+    
 }
 
-function playGame(){
-    for(i=0;i<5;i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection,computerSelection);
-    }
+/*function playGame(){
+    
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection,computerSelection);
+    
     if(humanScore==computerScore){
         console.log("It is a tie!!!");
     } else if(humanScore>computerScore){
@@ -63,7 +81,36 @@ function playGame(){
     } else{
         console.log("You lost :((");
     }
-}
+}*/
 
 
-playGame();
+let rockButton = document.createElement("button");
+let paperButton = document.createElement("button");
+let scissorButton = document.createElement("button");
+
+rockButton.textContent = "Rock";
+paperButton.textContent = "Paper";
+scissorButton.textContent = "Scissors";
+
+document.body.appendChild(rockButton);
+document.body.appendChild(paperButton);
+document.body.appendChild(scissorButton);
+
+let resultDiv = document.createElement("div");
+
+
+rockButton.addEventListener("click", () => {
+    resultDiv.innerHTML = playRound("ROCK");
+})
+
+paperButton.addEventListener("click",() => {
+    resultDiv.innerHTML = playRound("PAPER");
+})
+
+scissorButton.addEventListener("click",() => {
+    resultDiv.innerHTML = playRound("SCISSORS");
+})
+
+document.body.appendChild(resultDiv);
+
+
